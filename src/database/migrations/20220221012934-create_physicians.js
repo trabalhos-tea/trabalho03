7b7704d9926cd193835a01cfec726f5dacb3ec0b
Thyago Vinicius Camilo Cunha <thyago.vccunha@gmail.com>
2022-02-20 22:39:13 -0300

    feat: adicionar migration de criação da tabela de Physicians

---
+++ src/database/migrations/20220221012934-create_physicians.js
@@ -0,0 +1,32 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Physicians", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Physicians");
+  },
+};
