51cf43fdd4adec606a5e35d6d874032e82592287
Thyago Vinicius Camilo Cunha <thyago.vccunha@gmail.com>
2022-02-20 22:27:07 -0300

    chore: editar configuração de banco de dados

--- src/database/config/dbconfig.js
+++ src/database/config/dbconfig.js
@@ -1,23 +1,16 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+require("dotenv").config();
+
+const { DB_SERVER, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+
+module.exports = {
+  dialect: "mysql",
+  host: DB_SERVER,
+  port: DB_PORT,
+  username: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  timezone: "-03:00",
+  define: {
+    timestamp: true,
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+};
