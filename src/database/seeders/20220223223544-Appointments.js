e5bae2df177fed82cfdea0b2d1845998c5be071b
matheus-a-chaves <larissa.borges@ufpr.br>
2022-02-23 20:18:50 -0300

    criado seeders: Appointments

---
+++ src/database/seeders/20220223223544-Appointments.js
@@ -0,0 +1,56 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      'Appointments',
+      [
+        {
+          physicianId: 1,
+          patientId: 1,
+          appoinmentDate: '2022-02-23',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        },
+        {
+          physicianId: 1,
+          patientId: 2,
+          appoinmentDate: '2022-02-23',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+
+        },
+        {
+          physicianId: 2,
+          patientId: 1,
+          appoinmentDate: '2022-02-23',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+
+        },
+        {
+          physicianId: 2,
+          patientId: 4,
+          appoinmentDate: '2022-02-23',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+
+        },
+        {
+          physicianId: 3,
+          patientId: 2,
+          appoinmentDate: '2022-02-23',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+
+        },
+        {
+          physicianId: 3,
+          patientId: 6,
+          appoinmentDate: '2022-02-23',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        },
+      ],
+      {});
+
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Appointments', null, {});
+  }
+};
